05b3b1b63d45be369eb9c9a872be64472cd1a62d
starver20 <amarnarute2002@gmail.com>
2022-02-03 10:50:39 +0530

    Style: Updated progress bar for toast

--- components/toast/toast.js
+++ components/toast/toast.js
@@ -1,10 +1,13 @@
 const baseBtn = document.querySelector('#btn-baseline');
 const baselineToast = document.querySelector('.toast');
+const timer = document.querySelector('.timer');
 
 baseBtn.addEventListener('click', () => {
   baselineToast.style.display = 'flex';
+  timer.style.display = 'initial';
 
   setTimeout(() => {
     baselineToast.style.display = 'none';
+    timer.style.display = 'none';
   }, 4000);
 });
